b105c289e7b24f84dbb30a84af8460a61ea13b9c
Huang RenL <596372190@qq.com>
2020-03-06 22:11:31 +0800

    提交login

---
+++ src/views/Login/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id='login'>这里是登录页
+       
+    </div>
+</template>
+<script>
+export default{
+    name:'login'
+}
+</script>
+<style lang="scss" scoped>
+#login{
+    background-color: #344a5f;
+    height:100vh;
+}
+</style>
